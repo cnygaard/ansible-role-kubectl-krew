05dbf56b2f19ee952cea81fa4de6284ada5a0785
Christian Nygaard <christian@cnygaard.com>
2019-07-30 21:27:56 +0200

    Update Molecule testinfra test, remove unused default host test

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -6,15 +6,7 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-def test_hosts_file(host):
-    f = host.file('/etc/hosts')
-
-    assert f.exists
-    assert f.user == 'root'
-    assert f.group == 'root'
-
-
 def test_kubectl_krew(host):
     cmd = host.run("/usr/local/bin/kubectl krew version")
 
-    assert cmd.rc
+    assert cmd.rc == 0
